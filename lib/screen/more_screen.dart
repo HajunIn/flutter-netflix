d6e5737360d6d1d56f0b4cbf115f46271c3cf8e4
hajun <dlscjfry2010@naver.com>
2023-01-24 12:55:04 +0900

    더보기 및 파이어 베이스 json 파일 추가

--- lib/screen/more_screen.dart
+++ lib/screen/more_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MoreScreen extends StatelessWidget {
   @override
-  Widget build( BuildContext context) {
+  Widget build(BuildContext context) {
     return Container(
       child: Center(
         child: Column(
@@ -11,13 +13,66 @@ class MoreScreen extends StatelessWidget {
               padding: EdgeInsets.only(top: 50),
               child: CircleAvatar(
                 radius: 100,
-                backgroundImage: AssetImage('images/65242621_2486830611381038_915899340625543168_n.jpg'),
+                backgroundImage: AssetImage('images/test_movie_2.jpg'),
               ),
-            )
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              child: Text(
+                'TaeBbong',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(15),
+              width: 140,
+              height: 5,
+              color: Colors.red,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Linkify(
+                onOpen: (link) async {
+                  if (await canLaunch(link.url)) {
+                    await launch(link.url);
+                  }
+                },
+                text: "https://github.com/Hajun",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                linkStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: () {},
+                child: Container(
+                  color: Colors.red,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '프로필 수정하기',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
 }
